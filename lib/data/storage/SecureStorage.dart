a69d22c328a1f63b6d5d9982c6b1bd81a7d6a7e1
Abbas Hashem <abbashashem283@gmail.com>
2025-09-03 18:36:05 +0300

    secure storage is singleton now

--- lib/data/storage/SecureStorage.dart
+++ lib/data/storage/SecureStorage.dart
@@ -1,18 +1,26 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class SecureStorage {
-  final FlutterSecureStorage storage = const FlutterSecureStorage();
+  static final SecureStorage _instance = SecureStorage._internal();
+
+  factory SecureStorage() {
+    return _instance;
+  }
+
+  SecureStorage._internal();
+
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
 
   Future<String?> read(String key) async {
-    String? value = await storage.read(key: key);
+    String? value = await _storage.read(key: key);
     return value;
   }
 
   Future<void> write(String key, String data) async {
-    await storage.write(key: key, value: data);
+    await _storage.write(key: key, value: data);
   }
 
   Future<void> delete(String key) async {
-    await storage.delete(key: key);
+    await _storage.delete(key: key);
   }
 }
