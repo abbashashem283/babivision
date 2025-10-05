7c2f35d5a396f13397b00e04a0a142d48a2e84f4
Abbas Hashem <abbashashem283@gmail.com>
2025-10-05 20:28:34 +0300

    removed future builder from appointments in favor of manual state manipulation

--- lib/views/pages/Appointments.dart
+++ lib/views/pages/Appointments.dart
@@ -39,6 +39,8 @@ class _AppointmentsState extends State<Appointments> {
   List<Service>? _services;
   List<Clinic>? _clinics;
 
+  bool _isLoading = true, _isError = false, _isDone = false;
+
   String get optionUnselectedSvgUrl {
     if (_selectedService.id == -1) return "assets/icon-images/no-service.svg";
     if (_selectedClinic.id == -1) return "assets/icon-images/no-clinic.svg";
@@ -96,38 +98,68 @@ class _AppointmentsState extends State<Appointments> {
     );
   }
 
-  Future<Map<String, dynamic>> _getServicesWClinics() async {
+  void get _getServicesWClinics async {
     //debugPrint("getting services + clinics");
-    final servicesResponse = await Http.get("/api/services");
-    final Map<String, dynamic> jsonServices = jsonDecode(
-      servicesResponse.toString(),
-    );
-    // //debugPrint(jsonServices.toString());
-    final clinicsResponse = await Http.get("/api/clinics");
-    final Map<String, dynamic> jsonClinics = jsonDecode(
-      clinicsResponse.toString(),
-    );
-    //debugPrint("1");
-    List<Service> services =
-        (jsonServices["services"] as List)
-            .map((service) => Service.fromJson(service))
-            .toList();
-    //debugPrint("2");
-    List<Clinic> clinics =
-        (jsonClinics["clinics"] as List)
-            .map((clinic) => Clinic.fromJson(clinic))
-            .toList();
-    //debugPrint("3");
+    if (mounted) {
+      setState(() {
+        _isLoading = true;
+        _isDone = false;
+        _isError = false;
+      });
+    }
+    try {
+      final servicesResponse = await Http.get("/api/services");
+      final clinicsResponse = await Http.get("/api/clinics");
+      debugPrint(servicesResponse.toString());
+      final Map<String, dynamic> jsonServices = jsonDecode(
+        servicesResponse.toString(),
+      );
+      // //debugPrint(jsonServices.toString());
+      final Map<String, dynamic> jsonClinics = jsonDecode(
+        clinicsResponse.toString(),
+      );
+      //debugPrint("1");
+      List<Service> services =
+          (jsonServices["services"] as List)
+              .map((service) => Service.fromJson(service))
+              .toList();
+      //debugPrint("2");
+      List<Clinic> clinics =
+          (jsonClinics["clinics"] as List)
+              .map((clinic) => Clinic.fromJson(clinic))
+              .toList();
+      //debugPrint("3");
 
-    return {"services": services, "clinics": clinics};
+      _services = services;
+      _clinics = clinics;
+
+      if (mounted) {
+        setState(() {
+          _isLoading = false;
+          _isDone = true;
+          _isError = false;
+        });
+      }
+    } on Exception catch (_) {
+      // TODO
+      if (mounted) {
+        setState(() {
+          _isLoading = false;
+          _isDone = false;
+          _isError = true;
+        });
+      }
+    }
   }
 
   @override
   void initState() {
     // TODO: implement initState
+
+    super.initState();
     _selectedClinic = _noClinicPlaceHolder;
     _selectedService = _noServicePlaceHolder;
-    super.initState();
+    _getServicesWClinics;
   }
 
   @override
@@ -146,166 +178,161 @@ class _AppointmentsState extends State<Appointments> {
       ),
       body: Padding(
         padding: EdgeInsets.all(8),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              flex: 12,
-              child: B(
-                color: "tr",
-                child: SizedBox(
-                  width: double.infinity,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      FutureBuilder(
-                        future: _getServicesWClinics(),
-                        builder: (context, snapshot) {
-                          if (snapshot.hasData) {
-                            final Map<String, dynamic> data = snapshot.data!;
-                            _services = data["services"];
-                            _clinics = data["clinics"];
-                            return B(
-                              color: "tr",
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                    flex: 1,
-                                    child: B(
-                                      color: "tr",
-                                      child: _buildCombo(
-                                        value: _selectedService,
-                                        placeHolder: _noServicePlaceHolder,
-                                        items: _services!,
-                                        onChanged: (newValue) {
-                                          setState(() {
-                                            assert(
-                                              newValue != null,
-                                              "The new value of dropdown btn is null",
-                                            );
-                                            _selectedService = newValue!;
-                                          });
-                                        },
+        child:
+            _isLoading
+                ? Center(child: CircularProgressIndicator())
+                : Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      flex: 12,
+                      child: B(
+                        color: "tr",
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              B(
+                                color: "tr",
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      flex: 1,
+                                      child: B(
+                                        color: "tr",
+                                        child: _buildCombo(
+                                          value: _selectedService,
+                                          placeHolder: _noServicePlaceHolder,
+                                          items: _services!,
+                                          onChanged: (newValue) {
+                                            setState(() {
+                                              assert(
+                                                newValue != null,
+                                                "The new value of dropdown btn is null",
+                                              );
+                                              _selectedService = newValue!;
+                                            });
+                                          },
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                  Expanded(
-                                    flex: 1,
-                                    child: B(
-                                      color: "tr",
-                                      child: _buildCombo(
-                                        value: _selectedClinic,
-                                        placeHolder: _noClinicPlaceHolder,
-                                        items: _clinics!,
-                                        onChanged: (newValue) {
-                                          setState(() {
-                                            assert(
-                                              newValue != null,
-                                              "The new value of dropdown btn is null",
-                                            );
-                                            _selectedClinic = newValue!;
-                                          });
-                                        },
+                                    Expanded(
+                                      flex: 1,
+                                      child: B(
+                                        color: "tr",
+                                        child: _buildCombo(
+                                          value: _selectedClinic,
+                                          placeHolder: _noClinicPlaceHolder,
+                                          items: _clinics!,
+                                          onChanged: (newValue) {
+                                            setState(() {
+                                              assert(
+                                                newValue != null,
+                                                "The new value of dropdown btn is null",
+                                              );
+                                              _selectedClinic = newValue!;
+                                            });
+                                          },
+                                        ),
+                                        // child: SizedBox(width: 30, height: 30),
                                       ),
-                                      // child: SizedBox(width: 30, height: 30),
                                     ),
-                                  ),
-                                ],
-                              ),
-                            );
-                          }
-                          if (snapshot.hasError) {
-                            return Text(snapshot.error.toString());
-                          }
-                          return Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: CircularProgressIndicator(),
-                          );
-                        },
-                      ),
-                      Expanded(
-                        child:
-                            _selectedService.id == -1 ||
-                                    _selectedClinic.id == -1
-                                ? Center(
-                                  child: SvgPicture.asset(
-                                    optionUnselectedSvgUrl,
-                                    width: 150,
-                                    height: 150,
-                                  ),
-                                )
-                                : SingleChildScrollView(
-                                  child: FutureBuilder(
-                                    future: Http.get(
-                                      "/api/appointments?upto=3&clinic=${_selectedClinic.id}",
-                                    ),
-                                    builder: (context, snapshot) {
-                                      if (snapshot.hasData) {
-                                        final Map<String, dynamic> data =
-                                            jsonDecode(
-                                              snapshot.data.toString(),
-                                            );
-                                        debugPrint("1");
-                                        AppointmentManager
-                                        appointmentController =
-                                            AppointmentManager(
-                                              serviceTime:
-                                                  _selectedService.durationMin,
-                                              openTime:
-                                                  _selectedClinic.openTime,
-                                              closeTime:
-                                                  _selectedClinic.closeTime,
-                                              workdays: {
-                                                ..._selectedClinic.workDays,
-                                              },
-                                              appointments: data,
-                                            );
-                                        return Text(
-                                          appointmentController
-                                              .allAvailableAppointments(
-                                                '2025-10-06',
-                                                upto: 2,
-                                                day0Time: '11:00',
-                                              )
-                                              .toString(),
-                                        );
-                                      }
-                                      if (snapshot.hasError) {
-                                        return Text(snapshot.error.toString());
-                                      }
-                                      return Center(
-                                        child: CircularProgressIndicator(),
-                                      );
-                                    },
-                                  ),
+                                  ],
                                 ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: B(
-                color: "r",
-                child: FilledButton(
-                  onPressed: () {},
-                  style: FilledButton.styleFrom(backgroundColor: Colors.purple),
-                  child: Center(
-                    child: Text(
-                      "Book Appointment",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+                              ),
+                              Expanded(
+                                child:
+                                    _selectedService.id == -1 ||
+                                            _selectedClinic.id == -1
+                                        ? Center(
+                                          child: SvgPicture.asset(
+                                            optionUnselectedSvgUrl,
+                                            width: 150,
+                                            height: 150,
+                                          ),
+                                        )
+                                        : SingleChildScrollView(
+                                          child: FutureBuilder(
+                                            future: Http.get(
+                                              "/api/appointments?upto=3&clinic=${_selectedClinic.id}",
+                                            ),
+                                            builder: (context, snapshot) {
+                                              if (snapshot.hasData) {
+                                                final Map<String, dynamic>
+                                                data = jsonDecode(
+                                                  snapshot.data.toString(),
+                                                );
+                                                debugPrint("1");
+                                                AppointmentManager
+                                                appointmentController =
+                                                    AppointmentManager(
+                                                      serviceTime:
+                                                          _selectedService
+                                                              .durationMin,
+                                                      openTime:
+                                                          _selectedClinic
+                                                              .openTime,
+                                                      closeTime:
+                                                          _selectedClinic
+                                                              .closeTime,
+                                                      workdays: {
+                                                        ..._selectedClinic
+                                                            .workDays,
+                                                      },
+                                                      appointments: data,
+                                                    );
+                                                return Text(
+                                                  appointmentController
+                                                      .allAvailableAppointments(
+                                                        '2025-10-06',
+                                                        upto: 2,
+                                                        day0Time: '11:00',
+                                                      )
+                                                      .toString(),
+                                                );
+                                              }
+                                              if (snapshot.hasError) {
+                                                return Text(
+                                                  snapshot.error.toString(),
+                                                );
+                                              }
+                                              return Center(
+                                                child:
+                                                    CircularProgressIndicator(),
+                                              );
+                                            },
+                                          ),
+                                        ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                    Expanded(
+                      flex: 1,
+                      child: B(
+                        color: "r",
+                        child: FilledButton(
+                          onPressed: () {},
+                          style: FilledButton.styleFrom(
+                            backgroundColor: Colors.purple,
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Book Appointment",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
